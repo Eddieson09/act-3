9c64109a9fe9c5e21d24ac5139c6cc4efae118ea
Eddieson09 <eddiesongargaritano@gmail.com>
2025-07-30 01:34:46 +0000

    act

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Eddieson Gargaritano");
+Console.WriteLine("20221124352");
+Console.WriteLine("this is maybe a little bit difficult.");
