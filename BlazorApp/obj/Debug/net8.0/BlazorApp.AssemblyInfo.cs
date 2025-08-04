c09fbce59bf919fe2eabb35c2953d6f676194d4b
Eddieson09 <eddiesongargaritano@gmail.com>
2025-08-04 02:32:16 +0000

    hehe

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ecba315640a697e2c302ad98f97eda2c67b222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c64109a9fe9c5e21d24ac5139c6cc4efae118ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ OOPconsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using OOPConsole;
+
+namespace OOPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello,World!");
+            // Program2 name1 = new Program2();
+            // Program2 name2 = new Program2();
+            // name1.Hello("Santiago");
+            // name2.Hello("Taguro");
+
+            Vehicle kotse1 = new Vehicle();
+            Car kotse2 = new Car();
+            Car kotse3 = new Car();
+            
+        }
+
+    }
+    public abstract class Vehicle
+    {
+        public abstract void Travel();
+    }
+    public class Car : Vehicle
+    {
+
+        public string wheels;
+        public double color;
+        public int price;
+        public void UmaandarSa()
+        {
+            Console.WriteLine("Umaandar sa lupa");
+        }
+        public void InputData(string color, double price, int wheels)
+        {
+            this.color = color;
+            this.price = price;
+            this.wheels = wheels;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"The Cars color is {color} with the price of P{price}, and it has {wheels} wheels.");
+        }
+        public void Passenger(string name)
+        {
+            Console.WriteLine($"welcome {name}!");
+        }
+        public void Passenger(int number)
+        {
+            if (number < 5)
+            {
+                Console.WriteLine($"kasya kayong {number}");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Sobrang dami niyo na!");
+            }
+        }
+        public override void Travel()
+        {
+            Console.WriteLine("Travel by land");
+        }
+    }
+    
+}
