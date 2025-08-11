301cf21a54dfe59172c9b2aef9263e33eace2389
Eddieson09 <eddiesongargaritano@gmail.com>
2025-08-11 02:22:27 +0000

    act 5

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09fbce59bf919fe2eabb35c2953d6f676194d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af01843ffb7000401f2100d503be599b2a59fae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ OOPConsole3/Activity5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Activity5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape iskwer = new Square("Yellow", 10);
+            iskwer.DisplayInfo();
+            Console.WriteLine("Area." + iskwer.GetArea());
+            iskwer.Dispose();
+
+            Shape bilog = new Circle("Green", 5);
+            bilog.DisplayInfo();
+            Console.WriteLine("Area." + bilog.GetArea());
+            bilog.Dispose();
+        }
+    }
+
+    public abstract class Shape : IDisposable
+    {
+        public string Color { get; set; }
+        public double Dimension { get; set; }
+
+        public Shape(string color, double dimension)
+        {
+            Color = color;
+            Dimension = dimension;
+            Console.WriteLine("Shape Created");
+        }
+
+        public abstract double GetArea();
+        public abstract string Name { get; }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Shape: {Name}");
+            Console.WriteLine($"Color: {Color}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Shape Destroyed");
+            GC.SuppressFinalize(this);
+        }
+
+        ~Shape()
+        {
+            Console.WriteLine("Shape Finalized");
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(string color, double sideLength) : base(color, sideLength) { }
+
+        public override double GetArea()
+        {
+            return Dimension * Dimension;
+        }
+
+        public override string Name => "Square";
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(string color, double radius) : base(color, radius) { }
+
+        public override double GetArea()
+        {
+            return Math.PI * Dimension * Dimension;
+        }
+
+        public override string Name => "Circle";
+    }
+}
